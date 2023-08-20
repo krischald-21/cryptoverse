d5d5b39fffbc71cfc1162f7b3fc54a00b4dce0de
Krischal Dhungel <krischald21@gmail.com>
2023-08-20 20:02:37 +0545

    fix Page Routes

--- src/App.jsx
+++ src/App.jsx
@@ -22,20 +22,32 @@ const App = () => {
           <div className="routes">
             {/* Routes */}
             <Routes>
-              <Route path="/" exact component={Homepage} />
-              <Route path="/exchanges" exact component={Exchanges} />
+              <Route path="/" exact element={<Homepage />} />
+              <Route path="/exchanges" exact element={<Exchanges />} />
               <Route
                 path="/cryptocurrencies"
                 exact
-                component={Cryptocurrencies}
+                element={<Cryptocurrencies />}
               />
-              <Route path="/crypto/:coinId" exact component={CryptoDetails} />
-              <Route path="/news" exact component={News} />
+              <Route path="/crypto/:coinId" exact element={<CryptoDetails />} />
+              <Route path="/news" exact element={<News />} />
             </Routes>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CryptoVerse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link> |<Link to="/exchanges">Exchanges</Link> |
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
