c5f471da145bd43ea6e276e5e2d03fe06760983c
Krischal Dhungel <krischald21@gmail.com>
2023-08-21 20:07:23 +0545

    implement LineChart in CryptoDetails page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,7 +15,11 @@ import {
   ThunderboltOutlined,
 } from "@ant-design/icons";
 
-import { useGetCryptosDetailsQuery } from "../services/cryptoApi";
+import {
+  useGetCryptosDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi";
+import LineChart from "./LineChart";
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -23,9 +27,17 @@ const { Option } = Select;
 const CryptoDetails = () => {
   const { coinId } = useParams();
   const [timePeriod, setTimePeriod] = useState("7d");
+
+  // Fetching crypto details
   const { data, isFetching } = useGetCryptosDetailsQuery(coinId);
+
+  // Fetching crypto history
+  const { data: coinHistory } = useGetCryptoHistoryQuery({
+    coinId,
+    timePeriod,
+  });
+
   const cryptoDetails = data?.data?.coin;
-  console.log(cryptoDetails);
 
   if (isFetching) return "Loading...";
 
@@ -90,6 +102,7 @@ const CryptoDetails = () => {
   ];
 
   return (
+    // Basic Header info for selected Crypto
     <Col className="coin-detail-container">
       <Col className="coin-heading-container">
         <Title level={2} className="coin-name">
@@ -111,6 +124,12 @@ const CryptoDetails = () => {
         ))}
       </Select>
       {/* Chart */}
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
+      {/* Stats of Selected Crypto */}
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
@@ -129,6 +148,7 @@ const CryptoDetails = () => {
             </Col>
           ))}
         </Col>
+        {/* Stats of other cryptos */}
         <Col className="other-stats-info">
           <Col className="coin-value-statistics-heading">
             <Title level={3} className="coin-details-heading">
@@ -147,6 +167,7 @@ const CryptoDetails = () => {
           ))}
         </Col>
       </Col>
+      {/* Description of Crypto */}
       <Col className="coin-desc-link">
         <Row className="coin-desc">
           <Title level={3} className="coin-details-heading">
@@ -154,6 +175,7 @@ const CryptoDetails = () => {
             {HTMLReactParser(cryptoDetails.description)}
           </Title>
         </Row>
+        {/* Useful Links for the crypto */}
         <Col className="coin-links">
           <Title level={3} className="coin-details-heading">
             {cryptoDetails.name} Links
