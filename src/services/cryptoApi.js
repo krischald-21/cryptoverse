a5eb5b7ed19348b9455e2c504e6c3b1e22c3bdd8
Krischal Dhungel <krischald21@gmail.com>
2023-08-21 18:36:32 +0545

    create redux api for Crypto Details

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -18,8 +18,11 @@ export const cryptoApi = createApi({
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
+        }),
+        getCryptosDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
         })
     })
 })
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptosDetailsQuery } = cryptoApi;
